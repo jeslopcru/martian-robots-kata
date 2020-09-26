bed540d97a32a858b74032d90ef9929805acf025
jeslopcru <jeslopcru@gmail.com>
2020-09-26 17:47:32 +0200

    robot turn

--- src/robot/robot.js
+++ src/robot/robot.js
@@ -3,6 +3,15 @@ const turnLeft = command => {
   const index = orientations.indexOf(command);
   return command !== 'E' ? orientations[index + 1] : 'N';
 }
+
+const turnRight = command => {
+  const orientations = ['N', 'E', 'S', 'W'];
+  
+  const index = orientations.indexOf(command);
+  return command !== 'W' ? orientations[index + 1] : 'N';
+}
+
 module.exports = {
-  turnLeft
+  turnLeft,
+  turnRight
 }
--- src/robot/robot.test.js
+++ src/robot/robot.test.js
@@ -42,4 +42,43 @@ describe('robot', () => {
     expect(orientationResult).toEqual(expectedOrientation);
   });
 
+  it('should change to N, when actual orientation is W and turnRight', () => {
+
+    const actualOrientation = 'W';
+    const expectedOrientation = 'N';
+
+    const orientationResult = robot.turnRight(actualOrientation);
+
+    expect(orientationResult).toEqual(expectedOrientation);
+  });
+
+  it('should change to E, when actual orientation is N and turnRight', () => {
+
+    const actualOrientation = 'N';
+    const expectedOrientation = 'E';
+
+    const orientationResult = robot.turnRight(actualOrientation);
+
+    expect(orientationResult).toEqual(expectedOrientation);
+  });
+
+  it('should change to S, when actual orientation is E and turnRight', () => {
+
+    const actualOrientation = 'E';
+    const expectedOrientation = 'S';
+
+    const orientationResult = robot.turnRight(actualOrientation);
+
+    expect(orientationResult).toEqual(expectedOrientation);
+  });
+
+  it('should change to W, when actual orientation is S and turnRight', () => {
+
+    const actualOrientation = 'S';
+    const expectedOrientation = 'W';
+
+    const orientationResult = robot.turnRight(actualOrientation);
+
+    expect(orientationResult).toEqual(expectedOrientation);
+  });
 });
