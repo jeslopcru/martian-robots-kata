7de0143a988697d28c7226015107b4dd8fe0bc74
jeslopcru <jeslopcru@gmail.com>
2020-09-26 17:27:01 +0200

    use chart

--- src/mission/mission.js
+++ src/mission/mission.js
@@ -1,5 +1,10 @@
-const execute = mission => {
+const chart = require('./../chart/chart');
 
+const execute = mission => {
+  let chartData = chart.create(mission.map);
+
+  mission.robots[0].instructions.forEach(command => {
+  });
 }
 module.exports = {
   execute
