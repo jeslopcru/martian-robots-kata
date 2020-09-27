54a10c7e39059e0e98927f3c3bc8e127df6ac218
jeslopcru <jeslopcru@gmail.com>
2020-09-27 10:00:47 +0200

    add mission to index

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const parser = require('./src/parser/parser');
+const mision = require('./src/mission/mission');
 const fs = require('fs');
 
 const fileInput = './input/input.txt';
 
-const run = (fileInput) => {
+run = (fileInput) => {
   const text = fs.readFileSync(fileInput, 'utf-8');
   const data = parser.getMissionData(text);
+  const misionData = mision.execute(data)
 }
 
 run(fileInput);
--- src/mission/mission.js
+++ src/mission/mission.js
@@ -1,22 +1,27 @@
 const chart = require('./../chart/chart');
 const robot = require('./../robot/robot');
 
+function movement(command, robotData, chartData) {
+  if (command === 'R') {
+    robotData.position.o = robot.turnRight(robotData.position.o);
+  } else if (command === 'L') {
+    robotData.position.o = robot.turnLeft(robotData.position.o);
+  } else if (command === 'F') {
+    robotData.position = robot.moveFront(robotData.position);
+    if (robot.isLost(robotData.position, chartData)) {
+      robotData.isLost = true;
+    }
+  }
+  return robotData;
+}
+
 const execute = mission => {
   const chartData = chart.create(mission.map);
 
-  let initial = mission.robots[0];
+  const initial = mission.robots[0];
   let final = {position: mission.robots[0].position, isLost: false};
   initial.instructions.forEach(command => {
-    if (command === 'R') {
-      final.position.o = robot.turnRight(final.position.o);
-    } else if (command === 'L') {
-      final.position.o = robot.turnLeft(final.position.o);
-    } else if (command === 'F') {
-      final.position = robot.moveFront(final.position);
-      if (robot.isLost(final.position, chartData)) {
-        final.isLost = true;
-      }
-    }
+    final = movement(command, final, chartData);
   });
   return final;
 }
