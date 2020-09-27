14f524080e910acf8d8733775d1376a69537d693
jeslopcru <jeslopcru@gmail.com>
2020-09-27 09:57:10 +0200

    should obtain final position whe robot is Lost

--- src/mission/mission.js
+++ src/mission/mission.js
@@ -2,23 +2,23 @@ const chart = require('./../chart/chart');
 const robot = require('./../robot/robot');
 
 const execute = mission => {
-  let chartData = chart.create(mission.map);
+  const chartData = chart.create(mission.map);
 
   let initial = mission.robots[0];
-  let result = mission.robots[0];
+  let final = {position: mission.robots[0].position, isLost: false};
   initial.instructions.forEach(command => {
     if (command === 'R') {
-      result.position.o = robot.turnRight(initial.position.o);
+      final.position.o = robot.turnRight(final.position.o);
     } else if (command === 'L') {
-      result.position.o = robot.turnLeft(initial.position.o);
+      final.position.o = robot.turnLeft(final.position.o);
     } else if (command === 'F') {
-      result.position = robot.moveFront(
-        result.position,
-        chartData
-      );
+      final.position = robot.moveFront(final.position);
+      if (robot.isLost(final.position, chartData)) {
+        final.isLost = true;
+      }
     }
   });
-  return result.position;
+  return final;
 }
 module.exports = {
   execute
--- src/mission/mission.test.js
+++ src/mission/mission.test.js
@@ -12,11 +12,33 @@ describe('mission', () => {
       ]
     };
 
-    const expectedMissionResults = {x: 1, y: 1, o: 'E'};
-    
+    const expectedPositionResult = {x: 1, y: 1, o: 'E'};
+
 
     const missionResults = mission.execute(missionData);
 
-    expect(missionResults).toEqual(expectedMissionResults);
+    expect(missionResults.position).toEqual(expectedPositionResult);
+    expect(missionResults.isLost).toBeFalsy();
+  });
+
+  it('should obtain final position when robot move outside of chart', () => {
+    const missionData = {
+      map: {width: 5, height: 3},
+      robots: [
+        {
+          position: {x: 3, y: 2, o: 'N'},
+          instructions: ['F', 'R', 'R', 'F', 'L', 'L', 'F', 'F', 'R', 'R', 'F', 'L', 'L'],
+        }
+      ]
+    };
+
+    const expectedPositionResult = {x: 3, y: 3, o: 'N'};
+
+
+    const missionResults = mission.execute(missionData);
+
+    expect(missionResults.position).toEqual(expectedPositionResult);
+    expect(missionResults.isLost).toBeTruthy();
+
   });
 });
